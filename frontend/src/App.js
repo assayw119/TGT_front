6e041ad8193b7c44e83ce5d638556e0d1f82c96e
yaena1223 <yaena1223@dgu.ac.kr>
2022-10-03 22:05:47 +0900

    [DESIGN] Clubpage 레이아웃

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Mainpage from "./routes/Mainpage";
 import Post_list_page from "./routes/Post_list_page";
-import Navbar from "./component/Navbar";
+import Clubpage from "./routes/Clubpage";
+
 function App() {
     return (
         <div className="App">
@@ -9,6 +10,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Mainpage />}></Route>
                     <Route path="/Post_list" element={<Post_list_page />}></Route>
+                    <Route path="/Clubpage" element={<Clubpage />}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
