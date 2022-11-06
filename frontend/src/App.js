3b65a819bd3c45fe3f59dce7df91321195b89d7e
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-06 22:50:09 +0900

    feat: 캘린더 틀

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Mainpage from "./routes/Mainpage";
-import Post_list_page from "./routes/Post_list_page";
-import Clubpage from "./routes/Clubpage";
-import Login from "./routes/Login";
-import Logout from "./routes/Logout";
-import Writepage from "./routes/Writepage";
-import Signup from "./routes/Signup";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Mainpage from './routes/Mainpage';
+import PostListPage from './routes/PostListPage';
+import Clubpage from './routes/Clubpage';
+import Login from './routes/Login';
+import Logout from './routes/Logout';
+import Writepage from './routes/Writepage';
+import Signup from './routes/Signup';
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Mainpage />}></Route>
-                    <Route path="/Post_list" element={<Post_list_page />}></Route>
-                    <Route path="/Clubpage" element={<Clubpage />}></Route>
-                    <Route path="/Writepage" element={<Writepage />}></Route>
-                    <Route path="/Login" element={<Login />}></Route>
-                    <Route path="/Logout" element={<Logout />}></Route>
-                    <Route path="/Signup" element={<Signup />}></Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Mainpage />}></Route>
+          <Route path="/PostList" element={<PostListPage />}></Route>
+          <Route path="/Clubpage" element={<Clubpage />}></Route>
+          <Route path="/Writepage" element={<Writepage />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+          <Route path="/Logout" element={<Logout />}></Route>
+          <Route path="/Signup" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
